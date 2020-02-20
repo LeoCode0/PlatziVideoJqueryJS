4903d760cdcdcc8a5442b28347231e8bda57679d
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-02-19 23:48:26 -0600

    Probando las promesas

--- js/home.js
+++ js/home.js
@@ -1,1 +1,41 @@
 console.log("Hola mundo")
+let cambia = "Leo"
+
+function cambiarNombre(nombre){
+    cambia = nombre
+}
+
+const getUser1 = new Promise(function(todoBien, todoMal){
+    // llamar a un API
+    // setInterval
+    setTimeout(function(){
+        todoBien(`Se acabo el tiempo 5`)  
+    }, 3000)
+})
+
+const getUser = new Promise(function(todoBien, todoMal){
+    // llamar a un API
+    // setInterval
+    setTimeout(function(){
+        todoBien(`Se acabo el tiempo 3`)  
+    }, 3000)
+})
+
+// getUser
+//     .then(function(){
+//         console.log("Todo esta bien en la vida")
+//     })
+//     .catch(function(message){
+//         console.log(message)
+//     })
+
+Promise.race([
+    getUser1, 
+    getUser
+])
+.then(function(message){
+    console.log(message)
+})
+.catch(function(message){
+    console.log(message)
+})
