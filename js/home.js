917b1d380c9847e8af2d9f95601b50c1ac3e85ab
Leonardo Espinoza <leoespinozidk@gmail.com>
2020-02-22 11:07:38 -0600

    Ejercicio de API

--- js/home.js
+++ js/home.js
@@ -29,13 +29,58 @@ const getUser = new Promise(function(todoBien, todoMal){
 //         console.log(message)
 //     })
 
-Promise.race([
-    getUser1, 
-    getUser
-])
-.then(function(message){
-    console.log(message)
+// Promise.race([
+//     getUser1, 
+//     getUser
+// ])
+// .then(function(message){
+//     console.log(message)
+// })
+// .catch(function(message){
+//     console.log(message)
+// })
+
+$.ajax('https://randomuser.me/api/', {
+    method: 'GET',
+    success: function(data){
+        console.log(data)
+    },
+    error: function(error){
+        console.log(error)
+    }
 })
-.catch(function(message){
-    console.log(message)
-})
+
+fetch('https://randomuser.me/api/ssw')
+    .then(function(response){
+        // console.log(response)
+        return response.json()
+    })
+    .then(function(user){
+        console.log('user ', user.results[0].name.first)
+    })
+    .catch(function(){
+        console.log('Algo fallo')
+    });
+
+(async function load(){
+    // await 
+    // Action
+    // Terror
+    // Animation
+    async function getData(url){
+        const response = await fetch(`https://pokeapi.co${url}`)
+        const data = await response.json()
+        return data
+    }
+    const malePokemon = await getData('/api/v2/gender/male')
+    const femalePokemon = await getData('/api/v2/gender/female')
+    const undefinedPokemon = await getData('/api/v2/gender/3') 
+    // await getData('https://yts.mx/api/v2/list_movies.json?genre=terror')
+    //     .then(function (data){
+    //         console.log('terrorList', data)
+    //         terrorList = data
+    //     })
+    console.log('malePokemon', malePokemon)
+    console.log('femalePokemon' ,femalePokemon)
+    console.log('undefinedPokemon' ,undefinedPokemon)
+})()
